5a85d233a2f589c5b4d1a6c783415bfa57d5dac1
Rycia Soulblight <43384589+Rycia@users.noreply.github.com>
2023-11-21 00:29:46 -0600

    Add files via upload

---
+++ CombatMode/locales/zhCN.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CombatMode", "zhCN")
+if not L then
+    error("[Combat Mode] zhCN locale registration failed for Combat Mode.")
+    return
+end
+-- @localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
+
+--[[Translation missing --]]
+L["BUTTON_1_MACRO_DESC"] = "Enter the name of the action you wish to be ran here."
+--[[Translation missing --]]
+L["CURSEFORGE"] = "curseforge.com/wow/addons/combat-mode"
+--[[Translation missing --]]
+L["DISCORD"] = "discord.gg/5mwBSmz"
